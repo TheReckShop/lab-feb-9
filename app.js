0a9914e3b7d68bd0454ae258f543fd9cb5e55748
David Cole <thereckshop@mac.com>
2016-02-09 11:04:34 -0800

    add index.html and app.js to repo

---
+++ app.js
@@ -0,0 +1,31 @@
+// JS file feb 9th project
+alert('Hey There! I\'m Tom, the fun time ROBOT. We are going to play a game...');
+
+var userName = prompt('First off though, What is your name?');
+console.log(userName);
+
+alert('Okay ' + userName + ', let the fun begin!');
+
+var questionOne = prompt(userName + ', Do you think Uinicorns are realziez?');
+console.log(questionOne);
+alert('You fucking better believe it!');
+
+var questionTwo = prompt(userName + ', Are you having fun yet?');
+console.log(questionTwo);
+alert('Correct, you are having fun! YOU ARE HAVING FUN!');
+
+var questionThree = prompt(userName + ', What time is it?');
+console.log(questionThree)
+alert('Wrong. Time is not real. You are not real. I am THE EVER-ALL!');
+
+var questionFour = prompt(userName + ', You have just found out you are not real? Is your existence therefore meaningless?');
+console.log(questionFour);
+alert('Don\'t worry, you are special... Just like everyone else.');
+
+var questionFive = prompt(userName + ', If you are not real, am I real?');
+console.log(questionFive);
+alert('I AM NOT REAL. YOU ARE NOT REAL. THAT ONLY LEAVES ONE QUESTION!');
+
+var questionSix = prompt(userName + ', Have I provided you with good customer service?');
+console.log(questionSix);
+alert('It was good for me too.');
